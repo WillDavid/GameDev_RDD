042698a5bee61b98144f9bc3361ccf5fdcf2d0d4
WillDavid <wdma.lic17@uea.edu.br>
2020-08-19 20:02:20 -0300

    Add Pause Button - Function

--- RDD/Assets/Scripts/GameController.cs
+++ RDD/Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 
 public class GameController : MonoBehaviour
@@ -10,15 +11,28 @@ public class GameController : MonoBehaviour
     public Text movimentoRestantesText;
     public static GameController instance;
     // Start is called before the first frame update
-
+   
     public AudioSource backgroundSong;
+    public GameObject panelPause;
+    private GameObject cameraPosicao;
+    
+
+
    
     void Start()
     {
        instance = this;
        movimentoRestantesText.text = movimentosRestantes.ToString();
+       cameraPosicao = GameObject.Find("Camera Configurada");
        
+
+      
+    
     }
+
+
+    
+   
 
 
     public void TimeGame(){
@@ -35,5 +49,22 @@ public class GameController : MonoBehaviour
         movimentoRestantesText.text = movimentosRestantes.ToString();
         
     }
+
+    public void loadScene(string nomeCena){
+        SceneManager.LoadScene(nomeCena);
+        Time.timeScale = 1;
+    }
+
+    public void PainelPauseTrue(){
+      
+            //panelPause.transform.position = Vector2.MoveTowards(panelPause.transform.position, cameraPosicao.transform.position, 1400f * Time.deltaTime);
+        
+        
+    }
+
+
+
+
+
 
 }
